4b64be354eec2d0205f7a4da4147c007500941d1
Jangtaehun <jangth0056@naver.com>
2024-02-24 16:50:23 +0900

    #12.3, 복구완료

--- experiences/urls.py
+++ experiences/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Perks, PerkDetail
+from .views import Perks, PerkDetail, ExperienceList
 
 urlpatterns = [
+    path("list/", ExperienceList.as_view()),
     path("perks/", Perks.as_view()),
     path("perks/<int:pk>", PerkDetail.as_view()),
 ]
